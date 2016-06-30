ccbb1635a918226d93680e7e6e79a807e62f6363
Siew-Mai Chan <chansiewmai@gmail.com>
2016-06-30 13:39:14 +0800

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular.module('app', ['ionic'])
+
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+})();
